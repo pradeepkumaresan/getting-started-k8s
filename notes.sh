0fbb3a11946e702a9696cbdd64e4922a355d2758
Pradeep Kumaresan <pradeep.it39@gmail.com>
2021-02-22 15:50:51 +0530

    more changes

--- notes.sh
+++ notes.sh
@@ -8,10 +8,43 @@ docker image push pradeepkumaresan/getting-started-k8s:1.0
 kubectl apply -f pod.yml
 
 -- to get running container status
+kubectl get pods
 kubectl get pods --watch
 kubectl get pods -o wide
-kubectl describe pods tantalizing-tutankhamun
+kubectl get pods --show-labels
+kubectl describe pods hello-pod
 
 -- to delete a pod using pod name and yaml file
 kubectl delete pod nginx
 kubectl delete -f multi-pod.yml
+
+-- creating services imperatively
+kubectl expose pod hello-pod --name=hello-svc --target-port=8080 --type=NodePort
+
+-- list services
+kubectl get svc
+
+-- service info
+kubectl get service hello-svc --output yaml
+-- to find external ip
+kubectl get nodes --output wide
+
+-- to delete a service 
+kubectl delete svc hello-svc
+
+-- creating services declaratively (node port)
+kubectl apply -f svc-nodeport.yml
+
+-- describe a service
+kubectl describe svc ps-nodeport
+kubectl delete svc ps-nodeport
+
+-- creating services declaratively (load balancer)
+kubectl apply -f svc-lb.yml
+
+-- list deployments
+kubectl get deploy
+
+-- list replica sets
+kubectl get rs 
+
